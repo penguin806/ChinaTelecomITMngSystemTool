af5270da3077ee44f4326a40ea3d76f1e3999090
Snow <me@xuefeng.space>
2021-02-09 14:10:39 +0800

    Add comments

--- ChinaTelecomITMngSystemTool.py
+++ ChinaTelecomITMngSystemTool.py
@@ -1,14 +1,19 @@
+# v1.0	Feb 2nd, 2021 
+# Repo: https://github.com/penguin806/ChinaTelecomITMngSystemTool.git
 import requests
 import time
 import json
 from requests_html import HTMLSession
 
-jSessionId = "98FDFDF0B0FABCD59BAC36B02E21985D"
+#用户名
 userName = '0746tangr'
+# cookie
+jSessionId = "98FDFDF0B0FABCD59BAC36B02E21985D"
+# 时间间隔
 timeInterval = 10
 
-session = HTMLSession()
 def requestEventPage():
+	session = HTMLSession()
 	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'}
 	cookies = {"JSESSIONID": jSessionId}
 	url = 'http://134.176.30.19:9081/ITMS/newView/event/eventHandle.jsp'
@@ -17,7 +22,6 @@ def requestEventPage():
 	lockKey = result.html.find('#lockKey')
 	toolUrl = result.html.find('#toolUrl')
 
-	# print(result)
 	lockKey=lockKey[0].attrs['value']
 	toolUrl=toolUrl[0].attrs['value']
 
